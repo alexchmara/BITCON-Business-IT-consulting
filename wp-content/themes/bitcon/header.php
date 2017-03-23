4fe6043961ad96b33fc1f0a61282d7558efae405
Alex <alexandrapop@op.pl>
2017-03-23 12:45:07 +0100

    startind @media

--- wp-content/themes/bitcon/header.php
+++ wp-content/themes/bitcon/header.php
@@ -4,22 +4,24 @@
     <meta charset="utf-8">
     <title>BITCON | Business IT consulting</title>
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
-<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script>var templateUrl="<?php bloginfo('template_url'); ?>";</script>
     <script src="<?php bloginfo('template_url'); ?>/parallax.min.js"></script>
     <script src="<?php bloginfo('template_url'); ?>/app.js"></script>
-    <!-- <?php wp_head(); ?> -->
+    <?php wp_head(); ?>
   </head>
   <body>
     <header class="site-top">
         <div class="container">
+          <div class="hamburger">
+          </div>
+          <img class="logobitcon" src="<?php bloginfo('template_url'); ?>/images/logo1res.png" alt="BITCON">
           <ul class="menu logo">
-            <img src="<?php bloginfo('template_url'); ?>/images/logo1res.png" alt="BITCON">
-            <li><a href="#news-contact">Kontakt</a></li>
-            <li><a href="#offer">Usługi</a></li>
-            <li><a href="#description">Oferta</a></li>
+            <li class="nav"><a href="#news-contact">Kontakt</a></li>
+            <li class="nav"><a href="#offer">Usługi</a></li>
+            <li class="nav"><a href="#description">Oferta</a></li>
           </ul>
         </div>
     </header>
--- wp-content/themes/bitcon/template-parts/section-description.php
+++ wp-content/themes/bitcon/template-parts/section-description.php
@@ -3,6 +3,7 @@
         <div class="bg">
           <h1>OFERTA</h1>
           <h2>Pozyskiwanie dotacji unijnych dla mikro, małych i średnich przedsiębiorstw </h2>
+          <h4>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</h4>
         </div>
         <div class="triangle"></div>
         <div class="triangle"></div>
